d146a604e429c9644a3798b5bbe0cd710336969a
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-19 17:58:28 +0545

    partially secured

--- emailapp/settings.py
+++ emailapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,11 +30,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sachitkhadka73@gmail.com'
-EMAIL_HOST_PASSWORD = 'jevo ddig yzum hswr'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 
--- emailsend/views.py
+++ emailsend/views.py
@@ -1,27 +1,73 @@
-from django.shortcuts import render,redirect
-from .forms import EmailForm
-from django.template.loader import render_to_string
+# from django.shortcuts import render,redirect
+# from .forms import EmailForm
+# from django.template.loader import render_to_string
+# from django.conf import settings
+# from django.core.mail import send_mail
+
+
+# # Create your views here.
+# def EmailView(request):
+#     if request.POST:
+#         form=EmailForm(request.POST)
+
+#         if form.is_valid():
+#             name=(form.cleaned_data['name'])
+#             email=(form.cleaned_data['email'])
+#             content=(form.cleaned_data['message'])
+
+
+#             send_mail(name,content,settings.EMAIL_HOST_USER,[email],fail_silently=False)
+
+#             return redirect('emailsend:email')
+#     else:        
+#      form=EmailForm()
+
+#     return render(request,'emailsend/home.html',{
+#         'form':form
+#     })
+
+from django.core.mail import get_connection, send_mail
 from django.conf import settings
-from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from .forms import EmailForm
 
-
-# Create your views here.
 def EmailView(request):
-    if request.POST:
-        form=EmailForm(request.POST)
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
 
         if form.is_valid():
-            name=(form.cleaned_data['name'])
-            email=(form.cleaned_data['email'])
-            content=(form.cleaned_data['message'])
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            content = form.cleaned_data['message']
 
+            # Establish a connection to the SMTP server
+            connection = get_connection(
+                'django.core.mail.backends.smtp.EmailBackend',
+                username=settings.EMAIL_HOST_USER,
+                password=settings.EMAIL_HOST_PASSWORD,
+                fail_silently=False,
+            )
 
-            send_mail(name,content,settings.EMAIL_HOST_USER,[email],fail_silently=False)
+            try:
+                # Open the connection
+                connection.open()
+
+                # Send the email using the established connection
+                send_mail(
+                    name,
+                    content,
+                    settings.EMAIL_HOST_USER,
+                    [email],
+                    fail_silently=False,
+                    connection=connection,
+                )
+            finally:
+                # Close the connection after sending the email
+                connection.close()
 
             return redirect('emailsend:email')
-    else:        
-     form=EmailForm()
 
-    return render(request,'emailsend/home.html',{
-        'form':form
-    })
+    else:
+        form = EmailForm()
+
+    return render(request, 'emailsend/home.html', {'form': form})
